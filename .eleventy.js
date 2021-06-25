3686c9d50b66a43636a4bdaf49cbc4c06215842e
Victor Candiani <48362384+victorcandiani@users.noreply.github.com>
2021-06-24 23:02:15 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/admin')  
   
   eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    return DateTime.fromJSDate(dateObj).setLocale('pt-br').toLocaleString();
   })
 
   return {
